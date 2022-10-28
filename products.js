6d2e69e1eff278c76195a25692badb528c64b32f
Sheikh Niaz <niaj9182@gmail.com>
2022-10-28 23:29:55 +0600

    first commit

---
+++ products.js
@@ -0,0 +1,56 @@
+const products = [
+  {
+    id: 0,
+    name: "Food 1",
+    price: 29.99,
+    instock: 100,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/t1.jpg",
+  },
+  {
+    id: 1,
+    name: "Food 2",
+    price: 24.99,
+    instock: 43,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/t4.jpg",
+  },
+  {
+    id: 2,
+    name: "Food 3",
+    price: 19.99,
+    instock: 10,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/t3.jpg",
+  },
+  {
+    id: 3,
+    name: "Food 4",
+    price: 25.99,
+    instock: 5,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/t5.png",
+  },
+  {
+    id: 4,
+    name: "Food 5",
+    price: 29.99,
+    instock: 4,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/t6.jpg",
+  },
+  {
+    id: 5,
+    name: "Food 6",
+    price: 39.99,
+    instock: 40,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/t7.jpg",
+  },
+];
